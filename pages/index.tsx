fc455ec9a9f3ac39aa4326d797a24d0125e7404d
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-05-08 18:21:03 +0300

    chore: use /blog as main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,2 @@
-import type { NextPage } from 'next'
-
-const Home: NextPage = () => {
-	return <div>
-		This is my blog
-	</div>
-}
-
-export default Home
+export { getStaticProps } from './blog/index';
+export { default } from './blog/index';
