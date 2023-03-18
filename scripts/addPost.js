9bca356f7d33c0a9993d51f0c9c4f41ff537440e
vitonsky <86191922+vitonsky@users.noreply.github.com>
2023-03-18 14:32:02 +0300

    feat: add script to add post

---
+++ scripts/addPost.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const fs = require('fs');
+
+const date = new Date();
+const postDate = {
+	year: date.getUTCFullYear(),
+	month: date.toLocaleString('en-GB', { month: 'long' }).toLowerCase(),
+	day: date.getUTCDate(),
+};
+
+const template = `
+---
+title: Title
+time: '${date.toLocaleString('en-GB', { minute: '2-digit', hour: '2-digit' })}'
+tags:
+keywords:
+description:
+image:
+lang:
+---
+
+Text
+`.trimStart();
+
+const postUrlTitleSegment = String(date.getTime());
+
+const postsDir = path.resolve(__dirname, '../posts');
+const postDir = path.join(
+	postsDir,
+	String(postDate.year),
+	String(postDate.month),
+	postUrlTitleSegment,
+);
+const postFilename = `${postDate.year}-${date.toLocaleString('en-GB', {
+	month: '2-digit',
+})}-${postDate.day}-${postUrlTitleSegment}.md`;
+
+// Ensure post dir exists
+if (!fs.existsSync(postDir)) {
+	fs.mkdirSync(postDir, { recursive: true });
+}
+
+const postFilePath = path.resolve(postDir, postFilename);
+fs.writeFileSync(postFilePath, template);
