b5acc3147038fd2a1a2b1f2f17904f86c8509b1c
Robert Vitonsky <rob@vitonsky.net>
2024-05-02 14:22:13 +0200

    chore: remove aliases

--- server/services/createPostsRouter.ts
+++ server/services/createPostsRouter.ts
@@ -1,13 +1,13 @@
 import { Service } from '../types';
 
-import { getPost as getPostFn } from '../lib/posts';
+import { getPost } from '../lib/posts';
 import { GetPostRequest } from '../../common/api/GetPost';
 import { Router } from 'express';
-import { getPosts as getPostsFn } from '../lib/posts';
+import { getPosts } from '../lib/posts';
 import { GetPostsRequest } from '../../common/api/GetPosts';
-import { getPaginationInfo as getPaginationInfoFn } from '../lib/posts';
+import { getPaginationInfo } from '../lib/posts';
 import { GetPaginationInfoRequest } from '../../common/api/GetPaginationInfo';
-import { getPostUrls as getPostUrlsFn } from '../lib/posts';
+import { getPostUrls } from '../lib/posts';
 import { getPostWithAdditionalData } from '../lib/posts';
 import { GetPostWithAdditionalDataRequest } from '../../common/api/GetPostWithAdditionalData';
 
@@ -16,27 +16,27 @@ export const createPostsRouter: Service = () => {
 
 	router.get('/getPost', async (req, res) => {
 		const { url } = req.query as GetPostRequest;
-		const post = await getPostFn(url);
+		const post = await getPost(url);
 
 		res.send(post);
 	});
 
 	router.get('/getPosts', async (req, res) => {
 		const options = req.query as GetPostsRequest;
-		const posts = await getPostsFn(options);
+		const posts = await getPosts(options);
 
 		res.send(posts);
 	});
 
 	router.get('/getPaginationInfo', async (req, res) => {
 		const options = req.query as unknown as GetPaginationInfoRequest;
-		const paginationInfo = await getPaginationInfoFn(options);
+		const paginationInfo = await getPaginationInfo(options);
 
 		res.send(paginationInfo);
 	});
 
 	router.get('/getPostUrls', async (_, res) => {
-		const urls = await getPostUrlsFn();
+		const urls = await getPostUrls();
 
 		res.send(urls);
 	});
