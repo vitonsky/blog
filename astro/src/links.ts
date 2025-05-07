27e659a9fe03a3857fa6860472a987f6ef15101b
Robert Vitonsky <rob@vitonsky.net>
2025-05-07 04:58:59 +0200

    chore: add header

---
+++ astro/src/links.ts
@@ -0,0 +1,9 @@
+export const publicEmail = 'rob@vitonsky.net';
+
+export const socialMedia = {
+	mastodon: 'https://mastodon.social/@vitonsky',
+	twitter: 'https://twitter.com/rvitonsky',
+	github: 'https://github.com/vitonsky',
+	linkedin: 'https://www.linkedin.com/in/vitonsky',
+	stackoverflow: 'https://stackoverflow.com/users/18680275/vitonsky',
+} as const;
