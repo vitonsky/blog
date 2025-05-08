f1a25e6b19ff951e7e2a39631bd6eb2220d5dde2
Robert Vitonsky <rob@vitonsky.net>
2025-05-08 23:56:36 +0200

    chore: generate links from config

--- astro/src/links.ts
+++ astro/src/links.ts
@@ -1,9 +1,40 @@
-export const publicEmail = 'rob@vitonsky.net';
+import { getTagLink } from "./utils/links";
 
-export const socialMedia = {
-	mastodon: 'https://mastodon.social/@vitonsky',
-	twitter: 'https://twitter.com/rvitonsky',
-	github: 'https://github.com/vitonsky',
-	linkedin: 'https://www.linkedin.com/in/vitonsky',
-	stackoverflow: 'https://stackoverflow.com/users/18680275/vitonsky',
-} as const;
+type Link = {
+	text: string;
+	url: string;
+	title?: string;
+	target?: string;
+}
+
+export default {
+	head: [
+		{
+			text: "Home",
+			url: '/',
+		},
+		{
+			text: "Projects",
+			url: getTagLink('project'),
+		},
+		{
+			text: "About me",
+			url: "https://latexonline.cc/compile?git=https://github.com/vitonsky/resume&target=resume.tex&command=pdflatex",
+		},
+		{
+			text: "RSS",
+			url: "/rss.xml",
+		},
+	],
+	socials: [
+		{ text: "Twitter", url: 'https://twitter.com/rvitonsky', target: '_blank' },
+		{ text: "Mastodon", url: 'https://mastodon.social/@vitonsky', target: '_blank' },
+		{ text: "Github", url: 'https://github.com/vitonsky', target: '_blank' },
+		{ text: "LinkedIn", url: 'https://www.linkedin.com/in/vitonsky', target: '_blank' },
+		{ text: "StackOverflow", url: 'https://stackoverflow.com/users/18680275/vitonsky', target: '_blank' },
+		{ text: "Email", url: 'mailto:rob@vitonsky.net' },
+	],
+} as {
+	head: Link[];
+	socials: Link[];
+};
