bb435b2aa7fe20d9d1b0e2b273fa784382db3ead
Robert Vitonsky <rob@vitonsky.net>
2025-05-09 18:19:59 +0200

    chore: sort imports

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import shikiTheme from './shiki.json';
 
-import icon from 'astro-icon';
+import shikiTheme from './shiki.json';
 
 // https://astro.build/config
 export default defineConfig({
