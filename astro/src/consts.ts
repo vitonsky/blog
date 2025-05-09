87b4a74250213c08c84c13aff9fd583ec5be379d
Robert Vitonsky <rob@vitonsky.net>
2025-05-09 02:26:46 +0200

    chore: update meta tags

--- astro/src/consts.ts
+++ astro/src/consts.ts
@@ -1,2 +1,11 @@
 export const SITE_TITLE = 'Blog about computers';
 export const SITE_DESCRIPTION = 'Personal blog about computers and technologies';
+export const SITE_IMAGE = 'https://raw.githubusercontent.com/vitonsky/resume/refs/heads/master/face.jpeg';
+export const SITE_KEYWORDS = [
+	'programming',
+	'blog',
+	'management',
+	'code quality',
+	'frontend',
+	'open source',
+];
