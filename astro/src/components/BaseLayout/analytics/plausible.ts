7e787ba907652f78b32d66ef442fa9d460a321fd
Robert Vitonsky <rob@vitonsky.net>
2025-05-09 15:52:45 +0200

    chore: add plausible analytics package

---
+++ astro/src/components/BaseLayout/analytics/plausible.ts
@@ -0,0 +1,40 @@
+import Plausible from 'plausible-tracker';
+
+const setupPlausible = () => {
+	const plausible = Plausible({
+		domain: 'vitonsky.net',
+		apiHost: 'https://pulse2.vitonsky.net',
+	});
+
+	plausible.enableAutoPageviews();
+	plausible.enableAutoOutboundTracking();
+
+	document.body.addEventListener(
+		'click',
+		({ target }) => {
+			if (!(target instanceof HTMLAnchorElement)) return;
+			plausible.trackEvent('linkClick', {
+				props: {
+					// Current location
+					location: location.toString(),
+					url: target.href,
+					text: target.innerText,
+				},
+			});
+		},
+		{ capture: true },
+	);
+
+	return plausible;
+};
+
+// We use singleton instance, to share it between all islands
+let plausibleInstance: ReturnType<typeof Plausible> | null = null;
+export const getPlausible = () => {
+	// Init
+	if (!plausibleInstance) {
+		plausibleInstance = setupPlausible();
+	}
+
+	return plausibleInstance;
+};
