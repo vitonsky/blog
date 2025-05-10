743c64f18788b6a8b9ea84d8db3ae2dd2a9768ba
Robert Vitonsky <rob@vitonsky.net>
2025-05-10 17:28:22 +0200

    chore: use plausible-client

--- astro/src/components/BaseLayout/analytics/plausible/index.ts
+++ astro/src/components/BaseLayout/analytics/plausible/index.ts
@@ -1,4 +1,8 @@
-import { Plausible } from './Plausible';
+import {
+	enableAutoOutboundTracking,
+	enableAutoPageviews,
+	Plausible,
+} from 'plausible-client';
 
 const setupPlausible = () => {
 	const plausible = new Plausible({
@@ -6,32 +10,8 @@ const setupPlausible = () => {
 		apiHost: 'https://pulse2.vitonsky.net',
 	});
 
-	// Source: https://github.com/plausible/plausible-tracker/blob/ab75723ad10660cbaee3718d1b0a670e2dfd717d/src/lib/tracker.ts#L253-L284
-	const trackPageview = () => plausible.sendEvent('pageview');
-	const enableAutoPageviews = () => {
-		const page = () => trackPageview();
-		// Attach pushState and popState listeners
-		const originalPushState = history.pushState;
-		if (originalPushState) {
-			history.pushState = function (data, title, url) {
-				originalPushState.apply(this, [data, title, url]);
-				page();
-			};
-			addEventListener('popstate', page);
-		}
-
-		// Trigger first page view
-		trackPageview();
-
-		return function cleanup() {
-			if (originalPushState) {
-				history.pushState = originalPushState;
-				removeEventListener('popstate', page);
-			}
-		};
-	};
-
-	enableAutoPageviews();
+	enableAutoPageviews(plausible);
+	enableAutoOutboundTracking(plausible);
 
 	document.addEventListener(
 		'click',
