a528ff0de385d749f3860edc2cea334cad942592
Robert Vitonsky <rob@vitonsky.net>
2025-05-14 23:57:02 +0200

    chore: improve proofreader

--- scripts/proofread.ts
+++ scripts/proofread.ts
@@ -21,8 +21,19 @@ async function proofread(content: string): Promise<string> {
 		messages: [
 			{
 				role: 'system',
-				content:
-					'You are a proofreader. Fix grammar and style, preserve markdown. Do not add any comments and only return fixed markdown text',
+				content: `
+					You are a proofreader. Fix grammar and style, preserve markdown. Do not add any comments and only return fixed markdown text.
+
+					You must never replace computer terms.
+					You must never add a dots at end of lists.
+
+					Do not use character ";", prefer comma instead.
+
+					You must never change the tone of text. If text is a bit aggressive, you must leave this style.
+					You must never up promises. If text promises "to make great", you must never replace it to something like "to ensure great".
+					
+					You must not never move a dot inside quotemark like that \`"foo bar."\` as fix for \`"foo bar".\`
+					`,
 			},
 			{ role: 'user', content },
 		],
