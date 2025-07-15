4ea6b66ebbbbeb0f2546945d840dd203ee843073
Robert Vitonsky <robert@vitonsky.net>
2025-07-15 08:42:49 +0200

    chore: update post creation script

--- scripts/addPost.js
+++ scripts/addPost.js
@@ -25,24 +25,14 @@ keywords:
 
 // Write file
 
-const postDate = {
-	year: date.getUTCFullYear(),
-	month: date.toLocaleString('en-GB', { month: 'long' }).toLowerCase(),
-	day: date.getUTCDate(),
-};
-
-const postUrlTitleSegment = String(date.getTime());
-
 const postsDir = path.resolve(__dirname, '../posts');
 const postDir = path.join(
 	postsDir,
-	String(postDate.year),
-	String(postDate.month),
-	postUrlTitleSegment,
+	date.toLocaleString('en-GB', { year: 'numeric' }),
+	date.toLocaleString('en-GB', { month: '2-digit' }),
+	date.toLocaleString('en-GB', { day: '2-digit' }),
 );
-const postFilename = `${postDate.year}-${date.toLocaleString('en-GB', {
-	month: '2-digit',
-})}-${postDate.day}-${postUrlTitleSegment}.mdx`;
+const postFilename = `${String(date.getTime())}.mdx`;
 
 // Ensure post dir exists
 if (!fs.existsSync(postDir)) {
