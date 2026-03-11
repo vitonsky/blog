9f1b7ea917b31bfd23fc68df7a2d8ce7642bd605
Robert Vitonsky <robert@vitonsky.net>
2026-03-11 21:45:38 +0100

    chore: update plausible

--- astro/src/components/BaseLayout/analytics/plausible.ts
+++ astro/src/components/BaseLayout/analytics/plausible.ts
@@ -1,67 +1,30 @@
 import {
 	enableAutoOutboundTracking,
 	enableAutoPageviews,
+	enableEngagementTracking,
+	enableLinkClicksCapture,
+	enableSessionScoring,
+	filters,
 	Plausible,
+	skipByFlag,
+	skipForHosts,
+	userId,
 } from 'plausible-client';
-
-import { isExternalUrl } from '../../../utils/links';
 
 const setupPlausible = () => {
 	const plausible = new Plausible({
 		domain: 'vitonsky.net',
 		apiHost: 'https://uxt.vitonsky.net',
-		filter(event, eventName) {
-			// Skip all events while development
-			if (location.hostname === 'localhost') {
-				console.warn('Analytics event is skipped, since run on localhost', event);
-				return false;
-			}
-
-			// Skip all events for users who don't want to be tracked
-			if (window.localStorage.plausible_ignore === 'true') return false;
-
-			// Skip internal links
-			if (eventName === 'Outbound Link: Click') {
-				const url = event.props?.url;
-
-				if (typeof url !== 'string' || !isExternalUrl(url)) return false;
-			}
-
-			// Pass all events otherwise
-			return true;
-		},
+		filter: filters(skipForHosts(['localhost']), skipByFlag('plausible_ignore')),
+		transform: userId(),
 	});
 
 	enableAutoPageviews(plausible);
-	enableAutoOutboundTracking(plausible);
+	enableSessionScoring(plausible);
+	enableEngagementTracking(plausible);
 
-	document.addEventListener(
-		'click',
-		(event) => {
-			// Iterate over all targets to find Anchor element and take its text
-			// We do it instead of handle target, since click may appear on nested element
-			for (const node of event.composedPath()) {
-				if (!(node instanceof HTMLAnchorElement)) continue;
-
-				const rawText = node.innerText.trim();
-				const textLimit = 120;
-				const text =
-					rawText.length <= textLimit
-						? rawText
-						: rawText.slice(0, textLimit) + '...';
-
-				plausible.trackEvent('linkClick', {
-					props: {
-						// Current location
-						location: location.toString(),
-						url: node.href,
-						text,
-					},
-				});
-			}
-		},
-		{ capture: true },
-	);
+	enableAutoOutboundTracking(plausible, { captureText: true });
+	enableLinkClicksCapture(plausible, { captureText: true });
 
 	return plausible;
 };
